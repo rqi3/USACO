2416c0efaca509eac8a2077d9f21722b376db028
Richard <richardq343@gmail.com>
2023-10-13 18:31:44 -0400

    Update EgorJava1.java
    Short Java Code

--- Implementations/content/various/EgorJava1.java
+++ Implementations/content/various/EgorJava1.java
@@ -1,30 +1,71 @@
 /**
-* Description: Java Decimals.
-*/
+ * Description: Petr template
+ */
+import java.io.OutputStream; import java.io.IOException;
+import java.io.InputStream; import java.io.PrintWriter;
+import java.util.StringTokenizer; import java.util.*; 
+import java.io.IOException; import java.io.BufferedReader;
+import java.io.InputStreamReader; import java.io.InputStream;
+//Long arithmetics
+import java.math.BigInteger; import java.math.BigDecimal;
+//Math
+import java.math.MathContext; import java.math.RoundingMode;
 
-import java.util.*;
-import java.math.BigInteger;
-import java.math.BigDecimal;
-import java.math.MathContext;
-import java.math.RoundingMode;
-public class kek
-{
+public class Main {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        //Simple input/output
+        Scanner sc = new Scanner(System.in);
         long a = sc.nextLong();
-        long b = sc.nextLong();
-        long c = sc.nextLong();
-        long d = sc.nextLong();
+        System.out.println(new String("MEX"));
+
+        //BigDecimal
         BigDecimal e = new BigDecimal(a);
-        e = e.multiply(new BigDecimal(d));
-        BigDecimal f = new BigDecimal(c);
-        f = f.multiply(new BigDecimal(b));
-        if (e.compareTo(f) == 1) {
-            System.out.println(new String("Sasha"));
-        } else if (e.compareTo(f) == 0) {
-             System.out.println(new String("SashaAndKolya"));
-        } else {
-            System.out.println(new String("Kolya"));
+        e = e.multiply(new BigDecimal(a));
+        System.out.println(e);
+
+        //Vector, initial capacity 4
+        Vector<String> vec = new Vector<String>(4);
+
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        InputReader in = new InputReader(inputStream);
+        PrintWriter out = new PrintWriter(outputStream);
+        Taska solver = new Taska();
+        solver.solve(1, in, out);
+        out.close();
+    }
+    static class Taska {
+        int[][] array2d;
+        public void solve(int testNumber, InputReader in, PrintWriter out) {
+            int n = in.nextInt();
+            int[] a = new int[n];
+            int sum = 0;
+            for (int i = 0; i < n; ++i) {
+                a[i] = in.nextInt();
+                sum += a[i];
+            }
+            out.println(sum);
+        }
+    }
+    static class InputReader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+        public int nextInt() {
+            return Integer.parseInt(next());
         }
     }
 }
