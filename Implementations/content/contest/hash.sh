31b5003e9819c0759ce8c0eb984facfc77b389de
Richard <richardq343@gmail.com>
2025-08-24 23:02:07 -0700

    shorten to 26 pages

--- Implementations/content/contest/hash.sh
+++ Implementations/content/contest/hash.sh
@@ -1,5 +1,2 @@
-#!/bin/bash
-# Modern replacement for KACTL hash.sh
-# Usage: bash hash.sh < file.cpp
-
+# Modern replacement for KACTL hash.sh. Usage: bash hash.sh < file.cpp
 g++-13 -E -dD -P - <&0 2>/dev/null | tr -d '[:space:]' | md5sum | cut -c-6
