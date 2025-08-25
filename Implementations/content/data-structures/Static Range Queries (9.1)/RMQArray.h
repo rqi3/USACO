8a46e5943a629340aa833ffd8925beec3df7dba2
Richard <richardq343@gmail.com>
2025-08-24 22:04:55 -0700

    bring in fixes from master: new RMQArray and fixed ModMulLL

--- Implementations/content/data-structures/Static Range Queries (9.1)/RMQArray.h
+++ Implementations/content/data-structures/Static Range Queries (9.1)/RMQArray.h
@@ -1,26 +1,24 @@
 /**
- * Description: 1D range minimum query, with arrays.
+ * Description: Faster 1D range minimum query. 
  * Source: KACTL
+ * Verification: 
+	* https://judge.yosupo.jp/submission/126814
  * Memory: O(N\log N)
  * Time: O(1)
  */
 
-tcT, int SZ, int JMP> struct RMQ {
-	int level(int x) { return 31-__builtin_clz(x); }
-	T v[SZ]; 
-	T jmp[JMP][SZ];
-
-	void init() {
-		for(int i = 0; i < SZ; i++){
-			jmp[0][i] = v[i];
-		}
-		for (int j = 1; 1<<j <= SZ; ++j) {
-			int upper = SZ-(1<<j)+1;
-			for(int i = 0; i < upper; i++) jmp[j][i] = min(jmp[j-1][i], jmp[j-1][i+(1<<(j-1))]);
+tcT, size_t SZ> struct RMQ { // floor(log_2(x))
+	static constexpr int level(int x) { return 31-__builtin_clz(x); }
+	array<array<T,SZ>, level(SZ)+1> jmp;
+	T cmb(T a, T b) { return min(a, b); }
+	void init(const V<T>& v) { assert(sz(v) <= SZ);
+		copy(all(v), begin(jmp[0]));
+		for (int j = 1; 1<<j <= sz(v); ++j) {
+			F0R(i,sz(v)-(1<<j)+1) jmp[j][i] = cmb(jmp[j-1][i],
+				jmp[j-1][i+(1<<(j-1))]);
 		}
 	}
 	T query(int l, int r) {
 		assert(l <= r); int d = level(r-l+1);
-		return min(jmp[d][l],jmp[d][r-(1<<d)+1]); }
+		return cmb(jmp[d][l],jmp[d][r-(1<<d)+1]); }
 };
-// RMQ<int, 500000, 19> rmq;
