87748f8dd69291693e6fc93f2a04db7168feba07
Richard <richardq343@gmail.com>
2025-08-24 22:04:17 -0700

    Brought back in hash.sh and verified hashes Note: I think I used g++-13 but the hashes might not match up.  Modified run.sh to be g++-13 Put “unroll-loops” with “Ofast”

--- Implementations/content/contest/hash.sh
+++ Implementations/content/contest/hash.sh
@@ -1,4 +1,5 @@
-# Hash file ignoring whitespace and comments. Verifies that 
-# code was correctly typed. Usage: 'sh hash.sh < A.cpp'
-# cpp -dD -P -fpreprocessed|tr -d '[:space:]'|md5sum|cut -c-6
-cpp-12 -dD -P |tr -d '[:space:]'|md5sum|cut -c-6
+#!/bin/bash
+# Modern replacement for KACTL hash.sh
+# Usage: bash hash.sh < file.cpp
+
+g++-13 -E -dD -P - <&0 2>/dev/null | tr -d '[:space:]' | md5sum | cut -c-6
--- Implementations/content/contest/run.sh
+++ Implementations/content/contest/run.sh
@@ -1,3 +1,2 @@
-# Usage: sh run.sh L < L.in
-# Using g++-13 for Egor's computer (hashmap + Tree<int>)
+# Usage: bash run.sh L < L.in
 g++-13 -std=c++17 -O3 $1.cpp -o $1 && ./$1
